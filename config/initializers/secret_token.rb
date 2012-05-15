2d2cf7a8bb3ad21f9002921877f3203b9b71bbeb
Adrian Mejia <axm7988@rit.edu>
2012-05-14 20:35:23 -0400

    gems setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacetedSearch::Application.config.secret_token = 'dca67cfc8a7002603566e8e0e72f48fa0ea88375d2ee8e6b5741a3113f2adfb7d0833b5fc0271408bd8ba1b7fbd90b5fa616b8a544146f4007e083d0389e2fc1'
