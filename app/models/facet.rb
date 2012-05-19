496fffd958a382430e4fcc81bcda355144d59b05
Adrian Mejia <axm7988@rit.edu>
2012-05-19 19:30:58 -0400

    facet.rb brokend

--- app/models/facet.rb
+++ app/models/facet.rb
@@ -7,27 +7,53 @@ class Facet < ActiveRecord::Base
   private
     def self.create_hash(model)
       hash = {}
+      empty_values = 0
+      
       total = model.count
+      
       model.column_names.each do |name|
+        nil_values = 0
         # get options
-        options = model.select(name.to_sym)
+        options_name = model.select(name.to_sym)
           .uniq.map { |c| c.send(name) if c.send(name)}
-          .delete_if { |n| n.nil? || !(n =~ /\w/) }
+          .delete_if do |n|
+            nil_values +=1 
+            n.nil? || !(n =~ /\w/) 
+        end
+                
+        options = {}
+        options_name.each do |n|
+          count = Cable.where(name.to_sym => n).count
+          options[n] = count
+        end  
         
+        puts "29 options=#{options}"
+        puts "30 options_name=#{options_name}"
         hash[name] = { 
-          "relevance" => relevance(options.count, total),
+          "relevance" => relevance(options, total, nil_values),
           "options" => options 
         }
       end
-      #puts hash
       hash    
     end
     
-    def self.relevance(no_options, total)
-      no_options
+    def self.relevance(options, total, nil_values)
+      if total && total < 1
+        return 0.0
+      end
+      # get median percentage
+      puts "#{options}, #{total}, #{nil_values}"
+      median_per = median(options.values.map{|n| Float(n)*100/10 })
+      puts "median_per=#{median_per}"
+      puts "relevance (options.values.inject(:+)) = #{options.values.inject(:+)}"
+      options.values.inject(:+) * gauss(median_per/total)
     end
     
     def self.gauss(percentage)
       34/Math::sqrt(2*Math::PI*180)*Math::E**(-0.5*(percentage-50)**2/180)
+    end
+    
+    def self.median(array)
+      array.sort[array.count/2-1]
     end
 end
--- spec/models/facet_spec.rb
+++ spec/models/facet_spec.rb
@@ -12,52 +12,56 @@ describe Facet do
   end
   
   it "gets relevance for each column based on the data" do
-    
     # create 10 cables
     5.times do
       FactoryGirl.create(:cable)
       FactoryGirl.create(:cable2)
     end
     
-    
     # perform operation
     actual = Facet.calculate(Cable)
     
     describe "relevance calculations" do
       it "should have the correct number of options" do
-        actual["type"]["options"].should eq(["coax", "flat"])
+        actual["type"]["options"].keys.should eq(["coax", "flat"])
         actual["type"]["options"].count.should be 2
         actual["item_number"]["options"].count.should be 10
       end
       
       it "should omit empty options" do
-        actual["number_pairs"]["options"].should eq(["2"])
+        actual["number_pairs"]["options"].keys.should eq(["2"])
         actual["number_pairs"]["options"].count.should be 1
         actual["pitch"]["options"].count.should be 2
         actual["kevlar_core"]["options"].count.should be 1
       end
       
-      it "should get relevance=1 with all values equal" do
-        actual["filename"]["relevance"].should be_close 1, 0.1  
-      end
-      
       it "should get relevance > 1 for >1 distint values" do
-        actual["type"]["relevance"].should be_close 2, 0.1
+        actual["type"]["relevance"].should be_within(0.1).of(2)
       end
       
       it "should get relevance=0 with all null values" do
-        actual["item_description"]["relevance"].should be_close 0, 0.1
+        actual["item_description"]["relevance"].should be_within(0.1).of(0)
       end
       
       describe "Normal Distribution (Gauss)" do
+        it "should get relevance=0 with all values equal" do
+          actual["filename"]["relevance"].should be_within(0.1).of(0)  
+        end
+        
         it "should have a relevance=0 when all the values are the same" do
-          actual["item_number"]["relevance"].should be_close 0, 0.1
+          actual["item_number"]["relevance"].should be_within(0.1).of(0) #be_close 0, 0.1
         end
               
         it "return >1 for 50% of different values" do
-          actual["item_number"]["relevance"].should be_close 2, 0.1
+          actual["item_number"]["relevance"].should be_within(0.1).of(2) #be_close 2, 0.1
         end
          
+      end
+      
+      describe "show options count" do 
+        it "should have the options count" do
+          actual["type"]["options"].should eq({"coax"=>5, "flat"=>5})
+        end
       end
       
       it "should order by relevance"
