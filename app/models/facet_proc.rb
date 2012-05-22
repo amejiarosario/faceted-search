274c3e75d24993046b92f5f3f490cf1746c74407
Adrian Mejia <adriansky@gmail.com>
2012-05-22 17:17:51 -0400

    early bug found: don't use model.select to extract options in case the given data is a subset of the full database

--- app/models/facet_proc.rb
+++ app/models/facet_proc.rb
@@ -5,6 +5,7 @@ class FacetProc
     model.column_names.each do |column|
       f = Facet.new(column)
       #options names
+      # FIXME don't use model to select options... use the object! (in case is a subset)
       options = model.select(column.to_sym)
         .uniq.map { |c| c.send(column)}
         .delete_if { |n| n.nil? || n.blank? }
@@ -14,6 +15,7 @@ class FacetProc
       options.each do |n|
         options_hash[n] = model.where(column.to_sym => n).count
       end
+      
       f.options = options_hash
       arr << f
     end
