46a1eef9de88e1bd0fac89fe619177991b5f6a5c
Adrian Mejia <axm7988@rit.edu>
2012-05-22 00:03:57 -0400

    test suit in place

--- spec/models/facet_spec.rb
+++ spec/models/facet_spec.rb
@@ -27,12 +27,32 @@ describe Facet do
         v.should be_respond_to "name"
       end
     end
-    
+   
     it "options should be an array" do
       actual = FacetProc.calculate(Cable.all)
       actual.each do |v|
         v.options.class.should eq(Array)
       end      
+    end
+  end
+  
+  describe "FacetProc" do
+    it "should return an array of Facets with the column names"
+    it "should sort the elements by relevance"
+  end
+  
+  describe "Facet Objects" do
+    it "should return the right number of options(flat,coax,multi) for the column 'type'"
+    it "should have options count ('coax'=>4, 'flat'=>4, 'multi'=>2)"
+    
+    describe "calculate relevance" do
+      it "should have relevance=0 with 100% different values (item_number, mfg_part_number, id)"
+      it "should have relevance=10 with 50% different values (part_type column)"
+      it "should have relevance=0 with 0% different values (filename, level, conductor, wire_gauge)"
+      
+      it "should have relevance=0 with all null values (number_pairs, pitch)"
+      it "should have relevance=8 +/-2 with 40-40-30 values (type column)"
+      it "should have relevance=5 +/- 2 with 50% values and other null (number_pairs)"
     end
   end
   
