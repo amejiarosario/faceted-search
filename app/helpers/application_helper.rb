3a080ff53e35b4d150574f07cbd5555eaf704c9e
Adrian Mejia <axm7988@rit.edu>
2012-06-01 00:03:54 -0400

    will_paginate fix for bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     link_to title, params.merge(sort: column, direction: direction, page: nil), {class: css_class}    
   end
+  
+  #https://gist.github.com/1562185
+  def paginate *params
+    params[1] = {} if params[1].nil?
+    params[1][:renderer] = BootstrapPaginationHelper::LinkRenderer
+    will_paginate *params
+  end
+  
 end
