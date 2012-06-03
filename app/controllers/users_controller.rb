cc923fbc1041b7824ecb23e435ddcf75fbe9acae
Adrian Mejia <axm7988@rit.edu>
2012-06-02 20:19:27 -0400

    views for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,18 @@ class UsersController < ApplicationController
   load_and_authorize_resource
   
   def index
-    #@users = User.all
   end
   
   def show
-    #@user = User.find(params[:id])
   end
   
   def edit
+  end
+  
+  def new
+  end
+  
+  def create
   end
   
   def update
